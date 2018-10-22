139faaa01fa1843bb5762acece7805d1aab515af
Yunho Maeng <yunho.maeng.dc@gmail.com>
2018-10-22 23:03:42 +0900

    Update app.js

--- trufflebox-petshop/src/js/app.js
+++ trufflebox-petshop/src/js/app.js
@@ -29,7 +29,7 @@ App = {
             App.web3Provider = web3.currentProvider;
           } else {
             // If no injected web3 instance is detected, fall back to Ganache
-            App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+            App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
           }
           web3 = new Web3(App.web3Provider);
 
