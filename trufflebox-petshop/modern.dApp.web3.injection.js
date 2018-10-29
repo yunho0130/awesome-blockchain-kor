7a645a0c83538d021d4851ac256cb6f56ce7d2af
Yunho Maeng <yunho.maeng.dc@gmail.com>
2018-10-29 19:29:51 +0900

    Add modern dApp web3 injection

---
+++ trufflebox-petshop/modern.dApp.web3.injection.js
@@ -0,0 +1,24 @@
+window.addEventListener('load', async () => {
+    // Modern dapp browsers...
+    if (window.ethereum) {
+        window.web3 = new Web3(ethereum);
+        try {
+            // Request account access if needed
+            await ethereum.enable();
+            // Acccounts now exposed
+            web3.eth.sendTransaction({/* ... */});
+        } catch (error) {
+            // User denied account access...
+        }
+    }
+    // Legacy dapp browsers...
+    else if (window.web3) {
+        window.web3 = new Web3(web3.currentProvider);
+        // Acccounts always exposed
+        web3.eth.sendTransaction({/* ... */});
+    }
+    // Non-dapp browsers...
+    else {
+        console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+    }
+});
